14e84c21a8dc0c2b3a7b63d7cc4dc50f775be080
Mike Rybka <merybka@gmail.com>
2025-07-10 22:00:43 -0400

    Structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mikerybka/generate-schemas
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"path/filepath"
+
+	"github.com/mikerybka/util"
+)
+
+func main() {
+	schemasURL := "http://localhost:3000"
+	schemasDir := filepath.Join(util.HomeDir(), "schemas")
+	schemas, err := listSchemas(schemasURL)
+	if err != nil {
+		panic(err)
+	}
+	for _, e := range schemas.Value {
+		s, err := getSchema(schemasURL + "/" + e.Name)
+		if err != nil {
+			panic(err)
+		}
+		err = writeSchema(schemasDir, e.Name, s)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func getSchema(addr string) (*Schema, error) {
+	resp, err := http.Get(addr)
+	if err != nil {
+		return nil, err
+	}
+	res := &GetSchemaResponse{}
+	err = json.NewDecoder(resp.Body).Decode(res)
+	if err != nil {
+		return nil, err
+	}
+	return res.Value, nil
+}
+
+func writeSchema(dir string, name string, schema *Schema) error {
+	b, _ := json.Marshal(schema)
+	fmt.Println(string(b))
+	return nil
+}
+
+type ListSchemasResponse struct {
+	Value []DirEntry `json:"value"`
+}
+
+type GetSchemaResponse struct {
+	Value *Schema `json:"value"`
+}
+
+type DirEntry struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+type Schema struct {
+	Fields []Field `json:"fields"`
+}
+
+type Field struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+func listSchemas(schemasURL string) (*ListSchemasResponse, error) {
+	resp, err := http.Get(schemasURL)
+	if err != nil {
+		return nil, err
+	}
+	res := &ListSchemasResponse{}
+	err = json.NewDecoder(resp.Body).Decode(res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
